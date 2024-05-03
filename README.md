8085ac79b808ff299abb102979b4639acee940d3
Philipp Krüger <github.khab3@passinbox.com>
2024-05-03 15:25:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PyGWT - Python Git Worktree
+
+A CLI tool that's suppose to ease the `git worktree` workflow.
---
+++ src/pygwt/__main__.py
@@ -0,0 +1,10 @@
+"""Main Module."""
+
+
+def main() -> int:
+    """Entry Point for Main Module."""
+    return 0
+
+
+if __name__ == "__main__":
+    main()
